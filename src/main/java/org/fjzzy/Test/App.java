d6617f1dee2313dfa41240df4b0a5541df632a0b
hanhui12138 <1593423661@qq.com>
2015-12-26 18:43:00 +0800

    this is a test for git on eclipse

---
+++ src/main/java/org/fjzzy/Test/App.java
@@ -0,0 +1,13 @@
+package org.fjzzy.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
